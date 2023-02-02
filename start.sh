57195cd51befbaa03d6b3f2ee6a798a259a46320
Rk_botz <118276768+Rishi09090909@users.noreply.github.com>
2023-02-02 15:11:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rishi09090909/autofilterbot.git /websearch
+  git clone https://github.com/Rishi09090909/websearch.git /websearch
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /websearch
